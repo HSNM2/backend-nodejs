00bc7c57ab9449e5694e727336acd9ca50544034
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-04-26 07:26:10 +0800

    feat: 初始化專案

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOption = {
+  origin: '*',
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cookieParser())
+app.use(cors(corsOption))
+app.use(express.json())
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World!</h1>')
+})
+
+const port = process.env['PORT'] || 3002
+app.listen(port, () => {  
+  console.log(`The application listening listening on port ${port}`)
+})
