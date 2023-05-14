3be6aa9eb0c9a8aeedf71ca94518579fc2fa3649
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-05-14 08:43:00 +0800

    fix: 修正給予 env 方式
    1. 將 BACKEND_NODE_ENV 改回 NODE_ENV
    2. 因 db:migrate 抓取到系統環境變數，無法抓取到 .env 內參數
    3. 因此直接建立一個新的 db:migrate:prod 設定 -env production

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize')
-const env = process.env.BACKEND_NODE_ENV || 'development'
+const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../config/config.json')[env]
 
 let db = null
