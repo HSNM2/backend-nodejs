f017cb28ed45cc31e15b8e0d9be7865e6c04ed72
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-05-18 22:24:49 +0800

    feat: 新增 multer middle 在 /profile/pic/upload

--- routers/users.js
+++ routers/users.js
@@ -1,3 +1,4 @@
+const multer = require('multer')
 const express = require('express')
 const router = express.Router()
 const userController = require('controllers/userController')
@@ -12,7 +13,14 @@ router.post('/forgetPassword', userController.forgetPassword.post)
 router.get('/courses', userController.courses.get)
 router.get('/profile', [authJwt.verifyToken], userController.profile.get)
 router.patch('/profile', [authJwt.verifyToken], userController.profile.patch)
-router.post('/profile/pic/upload', [authJwt.verifyToken], pictureUpload.post)
+
+const uploadMiddleware = multer({ dest: 'uploads/' })
+router.post(
+  '/profile/pic/upload',
+  [authJwt.verifyToken, uploadMiddleware.single('file')],
+  pictureUpload.post
+)
+
 router.post('/tag/:courseId', [authJwt.verifyToken], userController.tag.post)
 router.delete('/tag/:courseId', [authJwt.verifyToken], userController.tag.delete)
 router.get('/identity', [authJwt.verifyToken], userController.identity.get)
