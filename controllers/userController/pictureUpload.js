cb9a05b47fe72fc9ead68dbf38e871c716f06784
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-05-19 21:33:40 +0800

    feat: 新增上傳大頭貼流程

--- controllers/userController/pictureUpload.js
+++ controllers/userController/pictureUpload.js
@@ -1,11 +1,15 @@
 const { errorTemplateFun } = require('src/utils/template')
+const { s3Uploadv3 } = require('../../src/js/s3Service')
 
 module.exports = {
   post: async (req, res) => {
     try {
+      const file = req.file
+      const result = await s3Uploadv3(file)
+
       return res.json({
         success: true,
-        data: 'upload'
+        data: result
       })
     } catch (error) {
       console.error(error)
--- routers/users.js
+++ routers/users.js
@@ -14,7 +14,18 @@ router.get('/courses', userController.courses.get)
 router.get('/profile', [authJwt.verifyToken], userController.profile.get)
 router.patch('/profile', [authJwt.verifyToken], userController.profile.patch)
 
-const uploadMiddleware = multer({ dest: 'uploads/' })
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.split('/')[0] === 'image') {
+    cb(null, true)
+  } else {
+    cb(new multer.MulterError('LIMIT_UNEXPECTED_FILE'), false)
+  }
+}
+const uploadMiddleware = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: fileFilter,
+  limits: { fileSize: 100000000 }
+})
 router.post(
   '/profile/pic/upload',
   [authJwt.verifyToken, uploadMiddleware.single('file')],
