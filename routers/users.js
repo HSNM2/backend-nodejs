d83b01bf816294f9c42a13bb969fd48bdb38ac47
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-05-21 11:04:53 +0800

    fix: 修正 multer.single 接受的 name
    1. 一般上傳圖片的 input tag 為 <input type="file" name="avatar">
    2. multer.single() 接收的第一個參數為 string，為止接受 name 的名稱
    3. 原先為 "file"，現在改為 avatar 更為語義化

--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const userController = require('controllers/userController')
 const pictureUpload = require('controllers/userController/pictureUpload')
 const { verifySignUp, authJwt } = require('middleware')
+const multer = require('multer')
 
 router.post('/register', [verifySignUp.checkDuplicateEmail], userController.register.post)
 router.post('/login', userController.login.post)
@@ -13,7 +14,7 @@ router.get('/profile', [authJwt.verifyToken], userController.profile.get)
 router.patch('/profile', [authJwt.verifyToken], userController.profile.patch)
 router.post(
   '/profile/pic/upload',
-  [authJwt.verifyToken, pictureUpload.uploadMiddleware.single('file')],
+  [authJwt.verifyToken, pictureUpload.uploadMiddleware.single('avatar')],
   pictureUpload.post
 ),
   router.delete('/profile/pic/upload', [authJwt.verifyToken], pictureUpload.delete)
