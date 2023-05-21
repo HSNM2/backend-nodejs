6665a6f4dfe6a80f64b19d17d45644203003030e
John <khgfe2002@gmail.com>
2023-05-21 18:52:13 +0800

    fit: 修補 命名錯誤

--- migrations/20230516053243-create-rating_summary-table.js
+++ migrations/20230516053243-create-rating_summary-table.js
@@ -38,6 +38,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('rating_summary')
+    await queryInterface.dropTable('rating_summarys')
   }
 }
--- migrations/20230516053256-create-rating_personal-table.js
+++ migrations/20230516053256-create-rating_personal-table.js
@@ -47,6 +47,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('rating_personal')
+    await queryInterface.dropTable('rating_personals')
   }
 }
