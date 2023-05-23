51759944be418088cc71e673eb26b2305e26277f
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-05-23 22:37:33 +0800

    fix: 修正是否為課程擁有者的判斷

--- middleware/verifyCourseProvide.js
+++ middleware/verifyCourseProvide.js
@@ -2,8 +2,9 @@ const { Course } = require('models/courses')
 
 const checkIsOwned = async (req, res, next) => {
   const { courseid } = req.params
-  const { userId } = req.body
+  const { userId } = req
   const course = await Course.findByPk(courseid)
+
   if (course.teacherId !== userId) {
     return res.status(403).json({ status: false, message: '你沒有權限獲取此課程資料' })
   }
