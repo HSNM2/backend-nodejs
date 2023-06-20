03143f1f34d27f91d5cbdb3d484322d00adeab61
John <khgfe2002@gmail.com>
2023-06-20 18:13:43 +0800

    refactor: 拆分課程詳細 api
    拆分課程詳細 api，拆為五支，分別為課程詳細、課程章節、課程常見問題、課前詢問、課程評價

--- routers/course.js
+++ routers/course.js
@@ -5,6 +5,10 @@ const courseController = require('controllers/courseController')
 const { verifySignUp, authJwt } = require('middleware')
 
 router.get('/:courseid', courseController.course.get)
+router.get('/:courseid/chapter', courseController.courseChapter.get)
+router.get('/:courseid/inquiry', courseController.courseInquiry.get)
+router.get('/:courseid/faq', courseController.courseFaq.get)
+router.get('/:courseid/rating', courseController.courseRating.get)
 router.post('/:courseid/preClassInquiry', [authJwt.verifyToken], courseController.inquiryAsk.post)
 router.post(
   '/:courseid/preClassInquiry/:inquiryid',
