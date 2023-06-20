a099d525d1f42221f1c47a0aa69fdad3df7a3e96
John <khgfe2002@gmail.com>
2023-06-20 12:56:38 +0800

    feat: 新增 顯示時間調整
    因伺服器時間不同，調整顯示時間。

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,13 +1,22 @@
 const CONVERT = {
   stringAddSymbol: (str) => `${str}！`,
   formatDate: (date) => {
-    const formattedDate = new Date(date).toLocaleDateString('zh-TW', {
+    // 因伺服器時區不是台灣時間
+    const localDate = new Date(date)
+    localDate.setHours(localDate.getHours() + 8) // 加上 8 小時以調整時區差異
+
+    // 處理超出 24 小時的情況
+    const hours = localDate.getHours()
+    const daysToAdd = Math.floor(hours / 24)
+    localDate.setDate(localDate.getDate() + daysToAdd)
+
+    const formattedDate = localDate.toLocaleDateString('zh-TW', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit'
     })
 
-    const formattedTime = new Date(date).toLocaleTimeString('zh-TW', {
+    const formattedTime = localDate.toLocaleTimeString('zh-TW', {
       hour: '2-digit',
       minute: '2-digit',
       hour12: false
