159a5ae20f1bfd7d1f83ba64c5de1c6bf46f78e0
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-06-22 10:21:59 +0800

    feat: 修正 line webhook 的 response

--- controllers/lineController/index.js
+++ controllers/lineController/index.js
@@ -1,10 +1,17 @@
 const { errorTemplateFun } = require('src/utils/template')
 
 exports.webhook = {
-  post: async (res, req) => {
+  post: async (req, res) => {
     try {
       console.log(`res: `, res)
       console.log(`req: `, req)
+
+      return res.json({
+        status: 200,
+        data: {
+          message: 'Line Webhook'
+        }
+      })
     } catch (error) {
       console.error(error)
       res.status(500).json(errorTemplateFun(error))
