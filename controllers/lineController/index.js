e256b1b45493c0746ea0c0bb86fa7ec8f7b82ec3
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-06-22 10:59:04 +0800

    feat: 安裝 @line/bot-sdk

--- controllers/lineController/index.js
+++ controllers/lineController/index.js
@@ -3,9 +3,7 @@ const { errorTemplateFun } = require('src/utils/template')
 exports.webhook = {
   post: async (req, res) => {
     try {
-      console.log(`res: `, res)
-      console.log(`req: `, req)
-
+      console.log(`events: `, req.body.events)
       return res.json({
         status: 200,
         data: {
--- routers/line.js
+++ routers/line.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const line = require('@line/bot-sdk')
 const router = express.Router()
 const lineController = require('controllers/lineController')
 
-router.post('/webhook', lineController.webhook.post)
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+}
+
+router.post('/webhook', line.middleware(config), lineController.webhook.post)
 
 module.exports = router
